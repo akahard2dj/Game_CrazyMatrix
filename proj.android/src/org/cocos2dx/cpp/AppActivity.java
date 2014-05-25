8b7a9f357d739080c9c590f97c8e383b14dfa8a5
정 도원 <motal@daum.net>
2014-05-25 19:40:35 +0900

    add admob

--- proj.android/src/org/cocos2dx/cpp/AppActivity.java
+++ proj.android/src/org/cocos2dx/cpp/AppActivity.java
@@ -23,52 +23,116 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
-****************************************************************************/
+ ****************************************************************************/
 package org.cocos2dx.cpp;
 
 import org.cocos2dx.lib.Cocos2dxActivity;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+
 import android.os.Bundle;
-import android.util.Log;
+import android.view.Gravity;
 import android.view.KeyEvent;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 public class AppActivity extends Cocos2dxActivity {
 
-	
-	
+	private AdView admobView;
+	Cocos2dxActivity mActivity;
+	LinearLayout layout;
+	LinearLayout mainLayout;
+	boolean isAdmobInited = false;
+
+	/* Your ad unit id. Replace with your actual ad unit id. */
+	private static final String AD_UNIT_ID = "ca-app-pub-6185986108413626/4819382398";
+
+	/** Called when the activity is first created. */
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		Log.e("jdw", "create!!");
-		
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		mActivity = this;
+
+		// Create an ad.
+		admobView = new AdView(this);
+		admobView.setAdSize(AdSize.BANNER);
+		admobView.setAdUnitId(AD_UNIT_ID);
+
+		showAdmob();
+	}
+
+	public void showAdmob() {
+		if (isAdmobInited || admobView == null) {
+			return;
+		}
+
+		mainLayout = new LinearLayout(mActivity);
+		mainLayout.post(new Runnable() {
+
+			@Override
+			public void run() {
+				AdRequest adRequest = new AdRequest.Builder().build();
+
+				WindowManager wm = (WindowManager) mActivity
+						.getSystemService("window");
+
+				WindowManager.LayoutParams mLayoutParams = new WindowManager.LayoutParams();
+				mLayoutParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_PANEL;
+				mLayoutParams.width = WindowManager.LayoutParams.WRAP_CONTENT;
+				mLayoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+				mLayoutParams.flags |= WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
+				mLayoutParams.gravity = Gravity.BOTTOM;
+
+				wm.addView(admobView, mLayoutParams);
+
+				// Enable this if your are testing AdMob, otherwise you'll risk
+				// to be banned!
+				// adRequest.addTestDevice (AdRequest.TEST_EMULATOR);
+				admobView.loadAd(adRequest);
+
+				isAdmobInited = true;
+			}
+		});
 	}
 
 	@Override
-	public void onBackPressed() {
-		Log.e("jdw", "sfsdfsf");
-		
-		super.onBackPressed();
-		Log.e("jdw", "sfsdfsf");
-		Log.e("jdw", "sfsdfsf");
-		Log.e("jdw", "sfsdfsf");
-		Log.e("jdw", "sfsdfsf");
-		
-		finish();
+	public void onResume() {
+		super.onResume();
+		if (admobView != null) {
+			admobView.resume();
+		}
+	}
+
+	@Override
+	public void onPause() {
+		if (admobView != null) {
+			admobView.pause();
+		}
+		super.onPause();
+	}
+
+	/** Called before the activity is destroyed. */
+	@Override
+	public void onDestroy() {
+		// Destroy the AdView.
+		if (admobView != null) {
+			admobView.destroy();
+		}
+		super.onDestroy();
 	}
 
 	@Override
 	public boolean onKeyUp(int keyCode, KeyEvent event) {
 
-		switch(keyCode) {
+		switch (keyCode) {
 		case KeyEvent.KEYCODE_BACK:
-			Log.e("jdw", "keycode_back");
 			finish();
 			break;
 		}
 		return super.onKeyUp(keyCode, event);
 	}
-	
 
 }
- 
